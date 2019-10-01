161692e9c0ecfe3450164759aeda66a2db77a7de
neekesh <nikesthapa20@gmail.com>
2019-10-01 23:45:24 +0545

    first version

--- api/app.py
+++ api/app.py
@@ -1,8 +1,41 @@
-from flask import Flask
-import json
+from flask import Flask, render_template,url_for, request, redirect
+from flask_sqlalchemy import  SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
-hash_data =  {'date':'epoch format', 'data':'I learned about web development topics (frontend, backend, db)'},{'date':'epoch format', 'data':'I found the falsk module'}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///til.db' #this is the route of the database
+db = SQLAlchemy(app)#intializing database
+
+class til(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  date_created = db.Column(db.DateTime, default = datetime.utcnow)
+  content = db.Column(db.String(2000), nullable= False)
+
+  def __repr__(self):
+    return '<TIl %r>' % self.id
+
+
+@app.route('/', methods=['GET', 'POST'])
+def submit():
+  if request.method == 'POST':
+    til_input = request.form['content']
+    new_input = til(content=til_input)
+
+    try:
+      db.session.add(new_input)
+      db.session.commit()
+      return redirect('/veiw')
+    except:
+      return 'There was an issue in storing your data'
+  else:
+
+    return render_template('submit.html')
 
 @app.route('/veiw')
-def json_data():
-  return json.dumps(hash_data)
+def veiw():
+  tild = til.query.order_by(til.date_created).all()
+
+  return render_template('veiw.html', tild=tild)
+
+if __name__ == '__main__':
+  app.run( debug=True)
